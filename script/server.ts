d4b84f0576e9b630fa188dea95adc2fd89eac9ad
allen_lyuqi <lyuqi@baidu.com>
2016-11-27 18:40:49 +0800

    push

--- script/server.ts
+++ script/server.ts
@@ -1,49 +1,18 @@
-var path = require('path');
-var http = require('http');
-var fs = require('fs');
 var express = require('express');
- 
 var app = express();
-var server = new http.Server(app);
+var path = require('path');
+app.set('port', (process.env.PORT || 5000));
 
+app.use(express.static(path.join(__dirname, '../../app')));
+app.set('views', path.join(__dirname, '../../app'));
+// views is directory for all template files
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
-var dll = fs.readFileSync('app/dll/library.dll.js', {
-    encoding: 'utf-8'
+app.get('/', function (request, response) {
+    response.render('index.html');
 });
-app.get('/dll/library.dll.js', function(req, res) {
-    res.status(200).send(dll);
-});
-app.get('/', function(req, res) {
-    var index = fs.readFileSync('app/index.html', {
-        encoding: 'utf-8'
-    });
-    res.status(200).send(index);
-});
-app.get('/img/*',  function(req, res) {
-    console.log(req.params)
-    var index = fs.readFileSync('app/img/'+req.params['0']);
-    res.status(200).send(index);
-});
-app.get('/bundle.js', function(req, res) {
-     var index = fs.readFileSync('app/bundle.js', {
-        encoding: 'utf-8'
-    });
-    res.status(200).send(index);
-});
-app.get('*', function(req, res) {
-    res.status(404).send('Server.js > 404 - Page Not Found');
-});
-app.use((err, req, res, next) => {
-    console.error("Error on request %s %s", req.method, req.url);
-    console.error(err.stack);
-    res.status(500).send("Server error");
-});
-process.on('uncaughtException', evt => {
-    console.log('uncaughtException ', evt);
-});
-server.listen('9000', (err) => {
-    if (err) {
-        console.error(err);
-    }
-    console.info('==> 💻  Open http://%s:%s in a browser to view the app.', 'localhost', '9000');
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
